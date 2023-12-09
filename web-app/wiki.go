78e8f7c5c1ea04515875d7b68d1cf20b457c86dd
Ahsan <ahsanaz461@gmail.com>
2023-12-09 16:44:34 +0700

    Make a PORT constant and Add indicators

--- web-app/wiki.go
+++ web-app/wiki.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -17,6 +18,8 @@ type Page struct {
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-z0-9]+)$")
+
+const PORT string = "8080"
 
 func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
 	m := validPath.FindStringSubmatch(r.URL.Path)
@@ -64,6 +67,7 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 
 // Handlers
 func handleView(w http.ResponseWriter, r *http.Request, title string) {
+	fmt.Println("Handling View localhost" + PORT)
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusNotFound)
@@ -74,6 +78,7 @@ func handleView(w http.ResponseWriter, r *http.Request, title string) {
 }
 
 func handleEdit(w http.ResponseWriter, r *http.Request, title string) {
+	fmt.Println("Handling Edit localhost" + PORT)
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -83,6 +88,7 @@ func handleEdit(w http.ResponseWriter, r *http.Request, title string) {
 }
 
 func handleSave(w http.ResponseWriter, r *http.Request, title string) {
+	fmt.Println("Handling Save localhost" + PORT)
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
@@ -95,9 +101,8 @@ func handleSave(w http.ResponseWriter, r *http.Request, title string) {
 }
 
 func main() {
-	const port string = ":8080"
 	http.HandleFunc("/view/", createHandler(handleView))
 	http.HandleFunc("/edit/", createHandler(handleEdit))
 	http.HandleFunc("/save/", createHandler(handleSave))
-	log.Fatal(http.ListenAndServe(port, nil))
+	log.Fatal(http.ListenAndServe(":"+PORT, nil))
 }
